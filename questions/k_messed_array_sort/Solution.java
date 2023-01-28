b71e5b14289769218fc97bbb03a4741593e17c68
n-ninjin <108375365+n-ninjin@users.noreply.github.com>
2023-01-28 17:16:32 -0500

    K-Messed Array Sort - Min heap approach

--- questions/k_messed_array_sort/Solution.java
+++ questions/k_messed_array_sort/Solution.java
@@ -1,6 +1,7 @@
 package questions.k_messed_array_sort;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
 /*
 K-Messed Array Sort
 
@@ -38,12 +39,46 @@ class Solution {
         return arr;
     }
 
+    static int[] sortKMessedArray2(int[] arr, int k) {
+        // create a min heap to store the first k+1 elements of the array
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        for (int i = 0; i <= k; i++) {
+            // add the elements to the heap
+            heap.offer(arr[i]);
+        }
+
+        int index = 0;
+        // iterate through the rest of the array
+        for (int i = k+1; i < arr.length; i++) {
+            // insert the smallest element from the heap into the array
+            arr[index++] = heap.poll();
+            // add the next element from the array to the heap
+            heap.offer(arr[i]);
+        }
+
+        // insert any remaining elements from the heap into the array
+        while (!heap.isEmpty()) {
+            arr[index++] = heap.poll();
+        }
+
+        return arr;
+    }
+
     public static void main(String[] args) {
         int[] arr = {1, 4, 5, 2, 3, 7, 8, 6, 10, 9};
         int k = 2;
+
+        // Insertion sort approach
         /* Expected output: {1, 2, 3, 4, 5, 6, 7, 8, 9, 10} */
         sortKMessedArray(arr, k);
         System.out.println(Arrays.toString(arr));
+
+        // Min heap approach
+        /* Expected output: {[0,1,2,3,4,5,6,7,8,9,10,11]} */
+        int[] arr2 = {6,1,4,11,2,0,3,7,10,5,8,9};
+        int k2 = 6;
+        sortKMessedArray2(arr2, k2);
+        System.out.println(Arrays.toString(arr2));
         // TC: O(N * K)
         // SC: O(1)
     }
